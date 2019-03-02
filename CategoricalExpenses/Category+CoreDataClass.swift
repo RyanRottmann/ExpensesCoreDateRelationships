f0221fb6390a73f73a611e5f637044a0d7cd8d2f
Ryan Rottmann <ryanrottmann@Ryans-MacBook-Pro.local>
2019-03-01 23:00:58 -0600

    First Commit

---
+++ CategoricalExpenses/Category+CoreDataClass.swift
@@ -0,0 +1,27 @@
+//
+//  Category+CoreDataClass.swift
+//  CategoricalExpenses
+//
+//  Created by Ryan Rottmann on 2/27/19.
+//  Copyright © 2019 Shawn Moore. All rights reserved.
+//
+//
+
+import UIKit
+import CoreData
+
+@objc(Category)
+public class Category: NSManagedObject {
+    var expenses: [Expense]? {
+        return self.rawExpenses?.array as? [Expense]
+    }
+    convenience init?(title: String){
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        guard let context = appDelegate?.persistentContainer.viewContext
+            else{
+                return nil
+        }
+        self.init(entity: Category.entity(), insertInto: context)
+        self.title = title
+    }
+}
